0b911c9101d441e2ef9c83f84b987fbb507da260
judith <judithi355@gmail.com>
2023-04-27 18:50:52 +0200

    removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // current date and time
 let newDateAndTime = new Date();
-let jsCurrentDateAndTime = document.querySelector(".currentDateAndTime");
+let jsCurrentDateAndTime = document.querySelector(".currentDayAndTime");
 let hour = newDateAndTime.getHours();
 if (hour < 10) {
-  let hour = `0${hour}`;
+  hour = `0${hour}`;
 }
 let minutes = newDateAndTime.getMinutes();
-/* if (minutes > 10) {
-  let minutes = `0${minutes}`;
-} */
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
 let weekDays = [
   "Sunday",
@@ -28,61 +28,30 @@ let form = document.querySelector("#search-form");
 
 function updatedCity(event) {
   event.preventDefault();
-  let searchCity = document.querySelector("#search-for-city");
-  let usersCity = document.querySelector("#city-name");
-  usersCity.innerHTML = `${searchCity.value}`;
+  let inputCityJs = document.querySelector("#input-city");
+  let usersCity = document.querySelector("#users-city-name-html");
+  usersCity.innerHTML = `${inputCityJs.value}`;
   let apiKey = `a95c2c6739994ba4903e007ee817e7d1`;
-
   let unit = `metric`;
-  let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&units=${unit}&appid=${apiKey}`;
+  let iconcode = ``;
+  let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${inputCityJs.value}&units=${unit}&${iconcode}@2x.png&appid=${apiKey}`;
   axios.get(`${weatherApi}`).then(updateWeather);
 }
 
 form.addEventListener("submit", updatedCity);
 
-// api response
+// api response for temp and city
 function updateWeather(response) {
+  // retreiving api city temp
   let temp = document.querySelector(".exactTemp");
-  temp.innerHTML = `${Math.round(
-    response.data.main.temp
-  )}<sup style="font-size:30px;">°C</sup>`;
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector(".weatherDescription").innerHTML =
-    response.data.weather[0].description;
+  temp.innerHTML = `${Math.round(response.data.main.temp)}`;
+  // retreiving api city name
+  document.querySelector(".cityPlaceholder").innerHTML = response.data.name;
+  // retreiving api city temp description
+  let weatherDescription = document.querySelector(".emojiSymbol");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  if (weatherDescription.innerHTML === `light rain`) {
+    weatherDescription.innerHTML = `🌧️`;
+  }
   console.log(response);
 }
-
-/*
-// get lat and long
-function revealPosition(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  let unit = `metric`;
-  let cLocation = document.querySelector("#current-location-button");
-  let apiKey = `a95c2c6739994ba4903e007ee817e7d1`;
-  let geolocationApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=0${long}&units=${unit}&appid=${apiKey}`;
-  axios.get(`${geolocationApi}`).then(revealGeoLo);
-
-  // geo location function here
-  function revealGeoLo(response) {
-    let areaName = response.data.name;
-    let usersCity = document.querySelector("#city-name");
-    usersCity.innerHTML = `${response.data.name}`;
-    let temp = Math.round(response.data.main.temp);
-    temp.innerHTML = `${Math.round(
-      response.data.main.temp
-    )}<sup style="font-size:30px;">°C</sup>`;
-  }
-}
-
-// current location button
-function revealLiveLocation(event) {
-  event.preventDefault();
-  let cLocation = document.querySelector("#current-location-button");
-}
-
-let liveLocationButton = document.querySelector("#current-location-button");
-liveLocationButton.addEventListener("click", revealLiveLocation);
-
-navigator.geolocation.getCurrentPosition(revealPosition);
-*/
